c071ab0de4c6dd19dbcf904d9d09fcb7daaaea1a
vipulpathak113 <vipulpathak113@gmail.com>
2023-12-28 16:54:18 +0530

    trim left

---
+++ src/00106-medium-trimleft.ts
@@ -0,0 +1,19 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<TrimLeft<"str">, "str">>,
+  Expect<Equal<TrimLeft<" str">, "str">>,
+  Expect<Equal<TrimLeft<"     str">, "str">>,
+  Expect<Equal<TrimLeft<"     str     ">, "str     ">>,
+  Expect<Equal<TrimLeft<"   \n\t foo bar ">, "foo bar ">>,
+  Expect<Equal<TrimLeft<"">, "">>,
+  Expect<Equal<TrimLeft<" \n\t">, "">>
+];
+
+// ============= Your Code Here =============
+type TrimLeft<S extends string> = S extends `${infer R}${infer U}`
+  ? R extends " " | "\n" | "\t"
+    ? TrimLeft<U>
+    : `${R}${U}`
+  : "";
