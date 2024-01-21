d9e0c33acecf3379b00c55eea1bf65e2a24e9a76
vipulpathak113 <vipulpathak113@gmail.com>
2024-01-21 23:17:08 +0530

    required keys

---
+++ src/00089-hard-required-keys.ts
@@ -0,0 +1,21 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<RequiredKeys<{ a: number; b?: string }>, "a">>,
+  Expect<Equal<RequiredKeys<{ a: undefined; b?: undefined }>, "a">>,
+  Expect<
+    Equal<
+      RequiredKeys<{ a: undefined; b?: undefined; c: string; d: null }>,
+      "a" | "c" | "d"
+    >
+  >,
+  Expect<Equal<RequiredKeys<{}>, never>>
+];
+
+// ============= Your Code Here =============
+type RequiredKeys<T> = {
+  [P in keyof T as T[P] extends Required<T>[P] ? P : never]: P;
+} extends infer R
+  ? R[keyof R]
+  : never;
